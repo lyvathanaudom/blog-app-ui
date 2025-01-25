73c8599523ab95e232b5608f7c2f4a9123459523
Vathana Park <90886658+lyvathanaudom@users.noreply.github.com>
2025-01-25 13:12:35 +0700

    add logo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,11 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap',
         },
+        {
+          rel: 'icon', // Use 'icon' for favicon or 'image/png' for other logos
+          type: 'image/png', // Adjust type based on your logo format
+          href: 'https://images.emojiterra.com/google/android-pie/512px/1f308.png' // Update with the correct path to your logo
+        },
       ],
     },
   },
